10f910c99b83bd9d97a2c686c07fab7cb8e1c794
andreas <rosenba@kth.se>
2018-11-07 14:13:15 +0100

    Reading data wih TFRecordDataset

--- data.py
+++ data.py
@@ -1,35 +1,7 @@
 import tensorflow as tf
 import os
 import matplotlib.pyplot as plt
-
-def read_and_decode(filename_queue):
-  reader = tf.TFRecordReader()
-  _, serialized_example = reader.read(filename_queue)
-
-  print(serialized_example.shape)
-  # Decode the record read by the reader
-  features = tf.parse_single_example(serialized_example, features = {
-        "image/encoded64": tf.FixedLenFeature([], tf.string),
-        "image/channels": tf.FixedLenFeature([], tf.int64),
-        "image/trainlable": tf.FixedLenFeature([], tf.float32),
-        "image/name": tf.FixedLenFeature([], tf.string),
-        "image/f1": tf.FixedLenFeature([], tf.string),
-        "image/f2": tf.FixedLenFeature([], tf.string),
-
-  })
-
-  trainlabel = tf.cast(features["image/trainlable"], tf.float32)
-
-  image_encoded64 = features["image/encoded64"]
-  image_raw64= tf.decode_raw(image_encoded64, tf.uint8)
-  image64 = tf.reshape(image_raw64,[64,64,2])
-
-  filename = tf.cast(features["image/name"], tf.string)
-  f1 = tf.cast(features["image/f1"], tf.string)
-  f2 = tf.cast(features["image/f2"], tf.string)
-
-  return image64, trainlabel, filename,f1,f2
-
+import numpy as np
 
 def parser(serialized_example):
   """Parses a single tf.Example into image and label tensors."""
@@ -51,80 +23,47 @@ def parser(serialized_example):
   filename = tf.cast(features["image/name"], tf.string)
   f1 = tf.cast(features["image/f1"], tf.string)
   f2 = tf.cast(features["image/f2"], tf.string)
-  return image64, trainlabel, filename,f1,f2
+  return image64, trainlabel 
 
 def read_dataset(DATASETNAME):
-    batch_size = 10
+    batch_size = 7
+    data = []
+    i = 1
     with tf.Session() as sess:
-      print("decoding tf file")
-      # filenames = [os.path.join(DATASETNAME +'.tfrecords')]
+        print("decoding tf file")
+        # filenames = [os.path.join(DATASETNAME +'.tfrecords')]
 
-      """ test how to make batch input data"""
-      filenames = tf.placeholder(tf.string, shape=[None])
-      dataset = tf.data.TFRecordDataset(filenames)
+        """ test how to make batch input data"""
+        filenames = tf.placeholder(tf.string, shape=[None])
+        dataset = tf.data.TFRecordDataset(filenames)
 
-      # Map the parser over dataset, and batch results by up to batch_size
-      dataset = dataset.map(parser,num_parallel_calls=None)
-      dataset = dataset.batch(batch_size)
-      dataset = dataset.repeat()
-      # iterator = dataset.make_one_shot_iterator()
-      iterator = dataset.make_initializable_iterator()
-      training_filenames = [os.path.join(DATASETNAME +'.tfrecords')]
-      sess.run(iterator.initializer, feed_dict={filenames: training_filenames})
+        # Map the parser over dataset, and batch results by up to batch_size
+        dataset = dataset.map(parser,num_parallel_calls=None)
+        #dataset = dataset.batch(batch_size)
+        #dataset = dataset.repeat()
+        iterator = dataset.make_initializable_iterator()
+        training_filenames = [os.path.join(DATASETNAME +'.tfrecords')]
+        sess.run(iterator.initializer, feed_dict={filenames: training_filenames})
 
-      image64, trainlabel, filename,f1,f2 = iterator.get_next()
-      print("image64 TYPE: ", type(image64))
-      print("img64 SIZE: ", image64.shape)
-      print("trainlabel TYPE:", type(trainlabel))
-      print("trainlabel SIZE:", trainlabel.shape)
-
-
-      print("#"*50)
-      # print(image64)
-      reconstructed_img64,re_filename,re_f1,re_f2,re_trainlabel = sess.run([image64,filename,f1,f2,trainlabel])
-      # print("reconstructed_img64 TYPE: ", type(reconstructed_img64))
-      # print("reconstructed_img64 SIZE: ", reconstructed_img64.shape)
-      # print("re_trainlabel TYPE:", type(re_trainlabel))
-      # print("re_trainlabel SIZE:", re_trainlabel.shape)
-
-      return reconstructed_img64, re_filename, re_f1, re_f2, re_trainlabel
-      """ the original code"""
-      # for f in filenames:
-      #       if not tf.gfile.Exists(f):
-      #           raise ValueError("Failed to find file: " + f)
-      # filename_queue = tf.train.string_input_producer(filenames)
-      #
-      # image64, trainlabel, filename,f1,f2 = read_and_decode(filename_queue)
-      #
-      # init_op = tf.initialize_all_variables()
-      # sess.run(init_op)
-      # coord = tf.train.Coordinator()
-      # threads = tf.train.start_queue_runners(coord=coord)
-
-      # for i in range(5):
-      #   reconstructed_img64,re_filename,re_f1,re_f2,re_trainlabel = sess.run([image64,filename,f1,f2,trainlabel])
-      #
-      #   print("reconstructed: " + str(re_filename) + " ,f1: " + str(re_f1) + " ,f2: " + str(re_f2) + " , trainlable: " + str(re_trainlabel) )
-      #
-      #   plt.subplot(1, 2, 1)
-      #   plt.imshow(reconstructed_img64[:,:,0])
-      #   plt.title('Object64')
-      #   plt.subplot(1, 2, 2)
-      #   plt.imshow(reconstructed_img64[:,:,1])
-      #   plt.title('grippers64')
-      #
-      #   plt.show()
-      # reconstructed_img64,re_filename,re_f1,re_f2,re_trainlabel = sess.run([image64,filename,f1,f2,trainlabel])
-      # print("reconstructed_img64 TYPE: ", type(reconstructed_img64))
-      # print("reconstructed_img64 SIZE: ", reconstructed_img64.shape)
-      # print("re_trainlabel TYPE:", type(re_trainlabel))
-      # print("re_trainlabel SIZE:", re_trainlabel.shape)
-      # coord.request_stop()
-      # coord.join(threads)
-
+        feature, label = iterator.get_next()
+        features = []
+        labels = []
+        try:
+            while True:
+                f, l = sess.run([feature, label])
+                features.append(f)
+                labels.append(l)
+                print("Has looped: ", i)
+                i+=1
+        except tf.errors.OutOfRangeError:
+            pass
+        npfeatures = np.array(features, dtype=np.int32)
+        nplabels = np.array(labels, dtype=np.int32)
+        print("Returning features and labels")
+    return npfeatures, nplabels
 
 def main():
-    DATASETNAME = "Data_science_dataset/Dataset_all_test"
+    DATASETNAME = "../../Data_science_dataset/Dataset_small_test"
     # test_tf_record(DATASETNAME)
     reconstructed_img64, re_filename, re_f1, re_f2, re_trainlabel=read_dataset(DATASETNAME)
     print("Printing in Main function:")
@@ -132,8 +71,6 @@ def main():
     print("reconstructed_img64 SIZE: ", reconstructed_img64.shape)
     print("re_trainlabel TYPE:", type(re_trainlabel))
     print("re_trainlabel SIZE:", re_trainlabel.shape)
-
-
 
 
 if __name__ == '__main__':
